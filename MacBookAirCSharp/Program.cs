5c0b3d3ba40589c761b75ae300ee7d1b8610a681
liebespaar93 <liebespaar93@gmail.com>
2021-10-01 22:09:02 +0900

    test

--- MacBookAirCSharp/Program.cs
+++ MacBookAirCSharp/Program.cs
@@ -14,7 +14,7 @@ namespace MacBookAirCSharp
                 number.Add(rand.Next(1000));
             }
             var fa = number.FindAll(x => (x % 2) == 0);
-
+            var test = 1;
             Console.WriteLine("fa type : {0}", fa.GetType());
             foreach (var x in fa)
                 Console.WriteLine(x);
